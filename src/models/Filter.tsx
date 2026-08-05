abe12fa06ebb652cc151188497c832d78fa4655f
blank0914 <yerin0373@gmail.com>
2026-08-05 21:04:38 +0900

    feat: UI 변경

--- src/models/Filter.tsx
+++ src/models/Filter.tsx
@@ -2,8 +2,8 @@ import type { ThreeElements } from "@react-three/fiber";
 
 export default function Filter() {
     return (
-        <mesh position={[1, 1.4, 0]}>
-            <cylinderGeometry args={[0.5, 0.5, 2, 32]} />
+        <mesh position={[1, 1, 0]}>
+            <cylinderGeometry args={[0.5, 0.5, 2.5, 32]} />
             <meshStandardMaterial color="#9CA3AF" />
         </mesh>
     );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,100 +1,27 @@
-import { useEffect, useState } from "react";
-import { getWeather } from "../api/weatherApi";
-import type { WeatherResponse } from "../types/WeatherResponse";
 import TwinScene from "../components/TwinScene";
 
 function HomePage() {
-    const [weatherData, setWeatherData] = useState<WeatherResponse | null>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-
-    const [showSensorData, setShowSensorData] = useState(false);
-    const [showRawData, setShowRawData] = useState(false);
-
-    useEffect(() => {
-        async function loadWeather() {
-            try {
-                setLoading(true);
-
-                const data = await getWeather();
-
-                setWeatherData(data);
-                setError(null);
-            } catch (error) {
-                console.error(error);
-                setError("기상청 데이터를 불러오지 못했습니다.");
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        loadWeather();
-    }, []);
-
-    if (loading) return <div>날씨 데이터를 불러오는 중...</div>;
-    if (error) return <div>{error}</div>;
-    if (!weatherData) return <div>데이터가 없습니다.</div>;
-
     return (
-        <div>
-            <h1>SRMS</h1>
-
-            <TwinScene/>
-
-            <h2>기상청 데이터</h2>
-
-            <p>기온 : {weatherData.weather.temperature}℃</p>
-            <p>습도 : {weatherData.weather.humidity}%</p>
-            <p>강수확률 : {weatherData.weather.rainfallProbability}%</p>
-            <p>강수량 : {weatherData.weather.rainfall}mm</p>
-
-            <br />
-
-            <p>데이터 출처 : 기상청 단기예보 조회서비스</p>
-
-            <p>
-                마지막 조회 :
-                {" "}
-                {weatherData.fetchedAt.toLocaleString()}
-            </p>
-
-            <hr />
-
-            <button
-                onClick={() => setShowSensorData(!showSensorData)}
-            >
-                {showSensorData
-                    ? "SensorData 숨기기"
-                    : "SensorData 보기"}
-            </button>
-
-            {
-                showSensorData && (
-                    <pre>
-                        {JSON.stringify(weatherData.sensors, null, 2)}
-                    </pre>
-                )
-            }
-
-            <br />
-            <br />
-
-            <button
-                onClick={() => setShowRawData(!showRawData)}
-            >
-                {showRawData
-                    ? "원본 데이터 숨기기"
-                    : "원본 데이터 보기"}
-            </button>
-
-            {
-                showRawData && (
-                    <pre>
-                        {JSON.stringify(weatherData.rawData, null, 2)}
-                    </pre>
-                )
-            }
-        </div>
+        <main className="page flex-center">
+            <h1 className="title">SRMS</h1>
+            <TwinScene />
+            <div className="card weather flex-center">
+                <h2 className="cardTitle">기상 정보</h2>
+                <p className="cardText">
+                    기온: <br />
+                    습도: <br />
+                    강수 확률: <br />
+                    강수량: <br />
+                </p>
+                <p className="cardText">
+                    데이터 출처: 기상청 단기예보 조회 서비스 <br />
+                    마지막 조회: 
+                </p>
+            </div>
+            <div className="card stat flex-center">
+                <h2 className="cardTitle">설비 상태</h2>
+            </div>
+        </main>
     );
 }
 
