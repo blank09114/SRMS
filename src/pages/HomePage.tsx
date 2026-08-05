f289898b3c4a58f9fe94fdff3c2ad8e89f9f1e39
blank0914 <yerin0373@gmail.com>
2026-08-05 21:18:15 +0900

    feat: ui 마무리

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,42 @@
+import { useEffect, useState } from "react";
+
 import TwinScene from "../components/TwinScene";
+import WeatherCard from "../components/WeatherCard";
+
+import { getWeather } from "../api/weatherApi";
+import type { WeatherResponse } from "../types/WeatherResponse";
 
 function HomePage() {
+    const [weatherData, setWeatherData] = useState<WeatherResponse | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function loadWeather() {
+            try {
+                setLoading(true);
+                setError(null);
+
+                const data = await getWeather();
+                setWeatherData(data);
+            } catch (error) {
+                console.error(error);
+                setError("기상 정보를 불러오지 못했습니다.");
+            } finally { setLoading(false); }
+        }
+
+        loadWeather();
+    }, []);
+
     return (
         <main className="page flex-center">
             <h1 className="title">SRMS</h1>
             <TwinScene />
-            <div className="card weather flex-center">
-                <h2 className="cardTitle">기상 정보</h2>
-                <p className="cardText">
-                    기온: <br />
-                    습도: <br />
-                    강수 확률: <br />
-                    강수량: <br />
-                </p>
-                <p className="cardText">
-                    데이터 출처: 기상청 단기예보 조회 서비스 <br />
-                    마지막 조회: 
-                </p>
-            </div>
+            <WeatherCard
+                weatherData={weatherData}
+                loading={loading}
+                error={error}
+            />
             <div className="card stat flex-center">
                 <h2 className="cardTitle">설비 상태</h2>
             </div>
