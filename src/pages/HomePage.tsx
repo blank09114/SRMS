75f70cdc8cbdbf48ba3b76480997c16d68059d60
blank0914 <yerin0373@gmail.com>
2026-08-05 09:30:48 +0900

    fix: api 예외 처리 개선

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,48 +1,67 @@
 import { useEffect, useState } from "react";
-
 import { getWeather } from "../api/weatherApi";
-
-import type { WeatherData } from "../types/WeatherData";
+import type { WeatherResponse } from "../types/WeatherResponse";
 
 function HomePage() {
-
-    const [weather, setWeather] = useState<WeatherData | null>(null);
+    const [weatherData, setWeatherData] = useState<WeatherResponse | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [showRawData, setShowRawData] = useState(false);
 
     useEffect(() => {
-
         async function loadWeather() {
-
             try {
+                setLoading(true);
                 const data = await getWeather();
-                setWeather(data);
+                setWeatherData(data);
+                setError(null);
             }
             catch (error) {
                 console.error(error);
+                setError("기상청 데이터를 불러오지 못했습니다.");
             }
-
+            finally { setLoading(false); }
         }
 
         loadWeather();
 
     }, []);
 
-    if (!weather) { return <div>날씨 데이터를 불러오는 중...</div>; }
+    if (loading) { return <div>날씨 데이터를 불러오는 중...</div>; }
+    if (error) { return <div>{error}</div>; }
+    if (!weatherData) { return <div>데이터가 없습니다.</div>; }
 
     return (
-
         <div>
-
             <h1>SRMS</h1>
 
-            <p>기온 : {weather.temperature}℃</p>
-            <p>습도 : {weather.humidity}%</p>
-            <p>강수확률 : {weather.rainfallProbability}%</p>
-            <p>강수량 : {weather.rainfall}mm</p>
+            <p>기온 : {weatherData.weather.temperature}℃</p>
+            <p>습도 : {weatherData.weather.humidity}%</p>
+            <p>강수확률 : {weatherData.weather.rainfallProbability}%</p>
+            <p>강수량 : {weatherData.weather.rainfall}mm</p>
 
+            <br/>
+
+            <p>데이터 출처: 기상청 단기예보 조회서비스</p>
+            <p>
+                마지막 조회:
+                {" "}
+                {weatherData.fetchedAt.toLocaleString()}
+            </p>
+
+            <br />
+
+            <button onClick={() => setShowRawData(!showRawData)}>
+                {showRawData ? "원본 데이터 숨기기" : "원본 데이터 보기"}
+            </button>
+
+            {
+                showRawData && (
+                    <pre> { JSON.stringify(weatherData.rawData, null, 2) } </pre>
+                )
+            }
         </div>
-
     );
-
 }
 
 export default HomePage;
